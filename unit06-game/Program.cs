cc3737de26636bf57b853bfec885c5a6d81c9551
Grant Watson <tnargw@gmail.com>
2022-12-03 18:14:59 -0700

    Changed format to reuse batter code

--- unit06-game/Program.cs
+++ unit06-game/Program.cs
@@ -1,27 +1,15 @@
 ﻿using System;
-using Raylib_cs;
+using Unit06.Game.Directing;
+using Unit06.Game.Services;
 
-namespace unit06_game
+namespace Unit06
 {
-  class Program
-  {
-    static void Main(string[] args)
+    public class Program
     {
-        Raylib.InitWindow(1200, 800, "Test") ;
-        Raylib_cs.Texture2D background = Raylib.LoadTexture("Assets/Images/test2.png");
-        Raylib_cs.Texture2D player = Raylib.LoadTexture("Assets/Images/player_image.png");  
-        
-        while (!Raylib.WindowShouldClose())
+        static void Main(string[] args)
         {
-            Raylib.BeginDrawing();
-            Raylib.ClearBackground(Color.WHITE);
-            Raylib.DrawTexture(background, 0, -100, Color.WHITE);
-            Raylib.DrawTexture(player, 1100, 0, Color.WHITE);
-            Raylib.EndDrawing();
-
-            
+            Director director = new Director(SceneManager.VideoService);
+            director.StartGame();
         }
-        Raylib.CloseWindow();
     }
-  }
 }
