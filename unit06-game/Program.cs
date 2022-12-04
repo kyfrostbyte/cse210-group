a180271134fddc9c19d42bd553c9659e9363add7
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2022-12-03 17:14:37 -0700

    fresh start

--- unit06-game/Program.cs
+++ unit06-game/Program.cs
@@ -1,15 +1,27 @@
 ﻿using System;
-using Unit06.Game.Directing;
-using Unit06.Game.Services;
+using Raylib_cs;
 
-namespace Unit06
+namespace HelloWorld
 {
-    public class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Raylib.InitWindow(1200, 800, "Test") ;
+        Raylib_cs.Texture2D background = Raylib.LoadTexture("Assets/Images/test2.png");
+        Raylib_cs.Texture2D player = Raylib.LoadTexture("Assets/Images/player_image.png");  
+        
+        while (!Raylib.WindowShouldClose())
         {
-            Director director = new Director(SceneManager.VideoService);
-            director.StartGame();
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.WHITE);
+            Raylib.DrawTexture(background, 0, -100, Color.WHITE);
+            Raylib.DrawTexture(player, 1100, 0, Color.WHITE);
+            Raylib.EndDrawing();
+
+            
         }
+        Raylib.CloseWindow();
     }
+  }
 }
